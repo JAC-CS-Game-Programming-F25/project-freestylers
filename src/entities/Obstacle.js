b851071106955fd34fa793904a1e2a8e81a5cdf9
bogdan.feher@gmail.com <bogdan.feher@gmail.com>
2025-12-13 09:50:56 -0500

    fixed obstacle to be using the rectangle's render instead of calling context

--- src/entities/Obstacle.js
+++ src/entities/Obstacle.js
@@ -1,48 +1,28 @@
 import Rectangle from './Rectangle.js';
 import Sprite from '../../lib/Sprite.js';
-import ImageName from '../enums/ImageName.js';
-import { world, images, context } from '../globals.js';
+import { images } from '../globals.js';
 
 export default class Obstacle extends Rectangle {
-    constructor(x, y, width, height, imageName) {
-    try {
-        super(x, y, width, height, {
-            density: 0.000000000000000000001,
-            friction: 0.5,
-            restitution: 0.3,
-            frictionAir: 0.9,
-            label: 'obstacle'
-        });
-        
-       
-        this.sprites = [
-            new Sprite(
-                images.get(imageName),
-                0, 0,
-                width,
-                height
-            )
-        ];
-        
-        console.log('Sprites created:', this.sprites);
-        this.currentFrame = 0;
-    } catch(error) {
-        console.error('Error in Obstacle constructor:', error);
-        throw error;
-    }
-}
+	constructor(x, y, width, height, imageName) {
+		super(x, y, width, height, {
+			isStatic: true,
+			density: 1e-21,
+			friction: 0.5,
+			restitution: 0.3,
+			frictionAir: 0.9,
+			label: 'obstacle',
+		});
 
-    render() {
-        super.render();
+		this.currentFrame = 0;
 
-        if (this.sprite) {
-            context.drawImage(
-                this.sprite,
-                this.body.position.x + this.renderOffset.x,
-                this.body.position.y + this.renderOffset.y,
-                this.width,
-                this.height
-            );
-        }
-    }
+		this.sprites = [
+			new Sprite(
+				images.get(imageName),
+				0,
+				0,
+				width,
+				height
+			),
+		];
+	}
 }
