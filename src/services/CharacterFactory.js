e497dd62a2124297e36979b4500a17d046e94e8a
bogdan.feher@gmail.com <bogdan.feher@gmail.com>
2025-12-15 11:27:00 -0500

    somewhat ok looking character factory

--- src/services/CharacterFactory.js
+++ src/services/CharacterFactory.js
@@ -5,28 +5,20 @@ import ImageName from "../enums/ImageName.js";
 
 export default class CharacterFactory {
     static createCharacter(x, y, flipped) {
+        const idleSpriteName = flipped ? ImageName.CyborgIdle : ImageName.PunkIdle;
+        const armSpriteName = flipped ? ImageName.CyborgHand : ImageName.PunkHand;
+        return new Character(x, y, CharacterFactory.getCharacterSprites(idleSpriteName, armSpriteName), flipped);
+    }
+
+    static getCharacterSprites(idleSpriteName, armSpriteName) {
         const idleSprite = new Sprite(
-            images.get(ImageName.PunkIdle),
+            images.get(idleSpriteName),
             196, 12, Character.WIDTH, Character.HEIGHT
         );
 
         const armSprite = new Sprite(
-            images.get(ImageName.PunkHand), 12, 12, 10, 15
+            images.get(armSpriteName), 12, 12, 10, 15
         );
-
-        return new Character(x, y, [idleSprite, armSprite], flipped);
-    }
-
-    /**
-     * Create random character
-     */
-    static createRandom(x, y) {
-        const types = [
-            () => CharacterFactory.createCharacter(x, y),
-            () => CharacterFactory.createCharacter(x, y),
-        ];
-        
-        const randomIndex = Math.floor(Math.random() * types.length);
-        return types[randomIndex]();
+        return [idleSprite, armSprite];
     }
 }
